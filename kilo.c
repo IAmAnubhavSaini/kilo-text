4afa8325edd9ef972319de2043383eecd6f68dff
Anubhav Saini <iamanubhavsaini+git@gmail.com>
2024-07-15 23:26:40 +0530

    quit on q
    Hmm, checkout this:
    ➜  kilo-text git:(main) ✗ make && ./kilo.exe
    cc -Wall -Werror -Wpedantic -Wextra -std=c11 kilo.c -o kilo.exe
    Hello from kilo!
    what is going on the quick dirty fox
    ➜  kilo-text git:(main) ✗ uick dirty fox
    Command 'uick' not found, did you mean:
      command 'ick' from deb intercal
      command 'uic' from deb qtchooser
      command 'pick' from deb mmh
      command 'pick' from deb nmh
      command 'pick' from deb mailutils-mh
      command 'pick' from deb pick
      command 'sick' from deb clc-intercal
      command 'uic3' from deb qtchooser
    Try: sudo apt install <deb name>

--- kilo.c
+++ kilo.c
@@ -5,7 +5,9 @@ int main() {
     printf("Hello from kilo!\n");
     char c;
     while(read(STDIN_FILENO, &c, 1) == 1) {
-        ;
+        if (c == 'q') {
+            break;
+        } 
     }
     return 0;
 }
